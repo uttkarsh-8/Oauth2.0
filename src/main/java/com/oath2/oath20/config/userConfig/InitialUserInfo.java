b3042f6fae6fd2db882d58cbbef9545ed36bce5c
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2024-05-15 03:36:40 +0530

    role based authorization implemented

--- src/main/java/com/oath2/oath20/config/userConfig/InitialUserInfo.java
+++ src/main/java/com/oath2/oath20/config/userConfig/InitialUserInfo.java
@@ -9,33 +9,33 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@RequiredArgsConstructor
-@Component
-public class InitialUserInfo implements CommandLineRunner {
-
-    private final UserInfoRepository userInfoRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    @Override
-    public void run(String... args) throws Exception {
-        UserInfoEntity manager  = new UserInfoEntity();
-        manager.setUsername("ManagerAAA");
-        manager.setPassword(passwordEncoder.encode("password"));
-        manager.setRoles("ROLE_MANAGER");
-        manager.setEmailId("manager@manager.com");
-
-        UserInfoEntity user = new UserInfoEntity();
-        user.setUsername("UserAAA");
-        user.setPassword(passwordEncoder.encode("password"));
-        user.setRoles("ROLE_USER");
-        user.setEmailId("user@user.com");
-
-        UserInfoEntity admin = new UserInfoEntity();
-        admin.setUsername("AdminAAA");
-        admin.setPassword(passwordEncoder.encode("password"));
-        admin.setRoles("ROLE_ADMIN");
-        admin.setEmailId("admin@admin.com");
-
-        userInfoRepository.saveAll(List.of(admin, manager, user));
-    }
-}
+//@RequiredArgsConstructor
+//@Component
+//public class InitialUserInfo implements CommandLineRunner {
+//
+//    private final UserInfoRepository userInfoRepository;
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        UserInfoEntity manager  = new UserInfoEntity();
+//        manager.setUsername("ManagerAAA");
+//        manager.setPassword(passwordEncoder.encode("password"));
+//        manager.setRoles("ROLE_MANAGER");
+//        manager.setEmailId("manager@manager.com");
+//
+//        UserInfoEntity user = new UserInfoEntity();
+//        user.setUsername("UserAAA");
+//        user.setPassword(passwordEncoder.encode("password"));
+//        user.setRoles("ROLE_USER");
+//        user.setEmailId("user@user.com");
+//
+//        UserInfoEntity admin = new UserInfoEntity();
+//        admin.setUsername("AdminAAA");
+//        admin.setPassword(passwordEncoder.encode("password"));
+//        admin.setRoles("ROLE_ADMIN");
+//        admin.setEmailId("admin@admin.com");
+//
+//        userInfoRepository.saveAll(List.of(admin, manager, user));
+//    }
+//}
--- src/main/java/com/oath2/oath20/controller/DashboardController.java
+++ src/main/java/com/oath2/oath20/controller/DashboardController.java
@@ -1,10 +1,35 @@
 package com.oath2.oath20.controller;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 public class DashboardController {
+
+    @PreAuthorize("hasAnyRole('ROLE_MANAGER', 'ROLE_ADMIN', 'ROLE_USER')")
+    @GetMapping("welcome-message")
+    public ResponseEntity<String> getFirstWelcomeMessage(Authentication authentication){
+        System.out.println(authentication.getAuthorities()+ " "+ authentication.getCredentials() );
+        return ResponseEntity.ok("Welcome to JWT oauth2.0:: "+ authentication.getName() + " with scope: " + authentication.getAuthorities());
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/admin-message")
+    public ResponseEntity<String> getAdminData(@RequestParam("message")String message, Principal principal){
+        return ResponseEntity.ok("Admin :: " + principal.getName()+ " has this message: " + message);
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    @GetMapping("manager-messaeg")
+    public ResponseEntity<String> getManagerData(Principal principal){
+        return ResponseEntity.ok("Manager :: " + principal.getName());
+    }
 }
